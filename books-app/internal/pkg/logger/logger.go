faf27417cbb4d1683f605d917e4f5791eb318396
Xavi Flores <xa_franc@live.com>
2025-05-20 15:38:22 -0600

    logger

--- books-app/internal/pkg/logger/logger.go
+++ books-app/internal/pkg/logger/logger.go
@@ -1,17 +1,74 @@
 package logger
 
 import (
-	_ "context"
-	_ "sync"
+	"context"
+	glog "log"
+	"sync"
+	"time"
 	_ "time"
 
-	_ "github.com/xavicci/gRPC-ModernAPI/books-app/internal/pkg/request"
+	"github.com/xavicci/gRPC-ModernAPI/books-app/internal/pkg/request"
 
 	"go.uber.org/zap"
-	_ "go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
 	Log   *zap.Logger
 	Sugar *zap.SugaredLogger
+
+	customTimeFormat string
+
+	onceInit sync.Once
 )
+
+func init() {
+	if err := Init(); err != nil {
+		glog.Fatalf("Failed to initialize logger: %v", err)
+	}
+}
+
+func Init() error {
+	var err error
+
+	onceInit.Do(func() {
+
+		cfg := zap.Config{
+			Encoding:         "json",
+			Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
+			OutputPaths:      []string{"stdout"},
+			ErrorOutputPaths: []string{"stdout"},
+			EncoderConfig: zapcore.EncoderConfig{
+				MessageKey:  "msg",
+				LevelKey:    "level",
+				EncodeLevel: zapcore.CapitalLevelEncoder,
+
+				TimeKey: "timestamp",
+				EncodeTime: zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+					enc.AppendString(t.UTC().Format("2006-01-02T15:04:05Z0700"))
+				}),
+
+				CallerKey:    "caller",
+				EncodeCaller: zapcore.ShortCallerEncoder,
+
+				StacktraceKey: "stacktrace",
+			},
+		}
+		Log, _ = cfg.Build()
+
+		Sugar = Log.Sugar()
+		zap.RedirectStdLog(Log)
+	})
+	return err
+}
+
+func addReqField(ctx context.Context) zapcore.Field {
+	return zap.String(request.RequestIDKey, request.GetContextRequestID(ctx))
+}
+
+func WithContext(ctx context.Context) *zap.SugaredLogger {
+	if ctx != nil {
+		return Sugar.With(addReqField(ctx))
+	}
+	return Sugar
+}
